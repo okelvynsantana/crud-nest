71eea23bfe40fd56f89e2788da78dae9140b1419
kelvynsantana <santanakelvyn@gmail.com>
2020-07-02 18:35:20 -0300

    CRUD task without DB persistence

--- src/tasks/shared/task.service.ts
+++ src/tasks/shared/task.service.ts
@@ -1,4 +1,51 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './task';
 
 @Injectable()
-export class TaskService {}
+export class TaskService {
+  tasks: Task[] = [
+    {id: 1, description: 'Tarefa 1', completed: false},
+    {id: 2, description: 'Tarefa 2', completed: true},
+  ];
+
+  getAll(): Task[] {
+    return this.tasks;
+  }
+
+  getById(id: number): Task{
+    const task = this.tasks.find((task) => task.id == id);
+    return task
+  }
+
+  create(task: Task): Task {
+    let lastId = 0;
+
+    if(this.tasks.length > 0) {
+      lastId = this.tasks[this.tasks.length - 1].id
+    }
+
+    task.id = lastId + 1;
+    this.tasks.push(task);
+
+    return task
+
+  }
+  update(task: Task): Task {
+    const taskArray = this.getById(task.id);
+
+    if(taskArray) {
+      taskArray.description = task.description;
+      taskArray.completed = task.completed;
+    }
+
+    return taskArray  ;
+  }
+
+  delete(id: number): void {
+    const index = this.tasks.findIndex((task) => task.id == id);
+
+    this.tasks.splice(index, 1);
+  }
+
+
+}
